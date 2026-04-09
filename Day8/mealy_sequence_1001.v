c56b3335b52ff38abe1d27577298beb8c3e68d3a
Vennila200405 <vennilas2004@gmail.com>
2026-04-09 14:17:06 +0530

    Refactor Mealy sequence state machine implementation

--- Day8/mealy_sequence_1001.v
+++ Day8/mealy_sequence_1001.v
@@ -3,54 +3,56 @@ module mealy_1001 (
     input rst,
     input x,
     output reg y);
-parameter S0 = 2'b00,
-              S1 = 2'b01,
-              S2 = 2'b10,
-              S3 = 2'b11;
+
+parameter s0 = 2'b00,
+          s1 = 2'b01,
+          s2 = 2'b10,
+          s3 = 2'b11;
 reg [1:0] state, next_state;
-  always @(posedge clk or posedge rst) begin
-        if (rst)
-            state <= S0;
-        else
-            state <= next_state;
-    end
-    always @(*) begin
-      case (state)
-            S0: begin
-                if (x == 1)
-                    next_state = S1;
-                else
-                    next_state = S0;
-                y = 0;
-            end
-              S1: begin
-                if (x == 0)
-                    next_state = S2;
-                else
-                    next_state = S1;
-                y = 0;
-            end
-                S2: begin
-                if (x == 0)
-                    next_state = S3;
-                else
-                    next_state = S1;
-                y = 0;
-                  end
-                  S3: begin
-                if (x == 1) begin
-                    next_state = S1; 
-                    y = 1;           
-                end else begin
-                    next_state = S2;
-                    y = 0;
-                end
-            end
-                    default: begin
-                next_state = S0;
-                y = 0;
-            end
-        endcase
-    end
+
+
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        state <= s0;
+    else
+        state <= next_state;
+end
+always @(*) begin
+     next_state = state;
+     y = 0;
+case (state)
+ s0: begin
+         if (x == 1)
+                next_state = s1;
+        end
+
+        s1: begin
+            if (x == 0)
+                next_state = s2;
+        end
+
+        s2: begin
+            if (x == 0)
+                next_state = s3;
+            else
+                next_state = s1;
+        end
+
+        s3: begin
+            if (x == 1) begin
+                next_state = s1;
+                y = 1;
+end
+            else
+                next_state = s0;
+        end
+
+        default: begin
+            next_state = s0;
+            y = 0;
+        end
+
+    endcase
+end
 
 endmodule
